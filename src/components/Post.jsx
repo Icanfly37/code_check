f11790477c6789bd3e682791e0bebcfdd9b0fb85
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2024-04-11 15:28:44 +0700

    .

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,19 @@
-function Post(Props) {
+/* eslint-disable react/prop-types */
+function Post(props) {
     function onClickLike (message){
         return (
             alert(message)
         );
     }
     return (
+        // eslint-disable-next-line react/prop-types
     <div className="post">
         <div className="title bg-blue">
-            <h3>{Props.title}</h3>
+            <h3>{props.title}</h3>
         </div>
         <div className="content">
-            <img className="pic_size" src={Props.img}></img>
-            <p>{Props.children}</p>
+            <img className="pic_size" src={props.img}></img>
+            <p>{props.children}</p>
         </div>
         <div>
             <button onClick={
